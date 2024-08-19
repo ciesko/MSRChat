fa1e72a92482fce54340a1e8d87b89981320943b
Christopher T Earley <jrekct@gmail.com>
2024-08-19 12:48:02 -0600

    add debug logging

--- build.sh
+++ build.sh
@@ -9,4 +9,9 @@ fi
 export POETRY_HTTP_BASIC_TNRDEV_USERNAME=agent
 export POETRY_HTTP_BASIC_TNRDEV_PASSWORD=$AZURE_ARTIFACTS_PAT
 
+set -x # Enable debugging
+
+echo "Username: $POETRY_HTTP_BASIC_TNRDEV_USERNAME"
+echo "Password: $POETRY_HTTP_BASIC_TNRDEV_PASSWORD"
+
 echo "pip authenticated to Azure Artifacts successfully."
